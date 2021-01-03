9349fd642de21c94ac8b0d8a94f3f1a250b8f8ff
Rohit Yadav <rohity123456@gmail.com>
2021-01-03 20:00:03 +0530

    styled error message

--- src/pages/error.js
+++ src/pages/error.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 const Error = () => {
   return (
-    <div>
-      <h1>404</h1>
-      <h3>Sorry, Page Not Found! </h3>
-      <Link to="/">Back to Home</Link>
+    <div className="error">
+      <div className="bottomShadowcontainer">
+        <h1>404</h1>
+        <h3>Sorry, Page Not Found! </h3>
+        <Link to="/">Back to Home</Link>
+      </div>
     </div>
   );
 };
